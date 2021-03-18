4e2027a19d408d48793324eae72f50c75d0a97cf
Dan Roscigno <dan@roscigno.com>
2021-03-18 16:22:23 -0400

    example using self managed deployment with self-signed cert

---
+++ connect-self-managed-self-signed-cert.js
@@ -0,0 +1,24 @@
+'use strict'
+const tls = require('tls');
+const fs = require('fs');
+
+const { Client } = require('@elastic/elasticsearch')
+const config = require('config');
+const elasticConfig = config.get('elastic-self-managed');
+
+const client = new Client({ 
+  node: elasticConfig.node,
+  auth: {
+    username: elasticConfig.username,
+    password: elasticConfig.password
+  },
+  ssl: {
+    ca: fs.readFileSync('../my-forks/ELK-Stack-with-Vagrant-and-Ansible/files/certs/ca/ca.crt'),
+    rejectUnauthorized: false
+  }
+})
+
+client.info()
+  .then(response => console.log(response))
+  .catch(error => console.error(error))
+
